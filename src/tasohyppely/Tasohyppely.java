69b3894aa9c9fbecf2b2c5cd19556ae273ae245f
Emil Laine <laine.emil@gmail.com>
2014-12-26 22:29:31 +0200

    piirtämistä...

--- src/tasohyppely/Tasohyppely.java
+++ src/tasohyppely/Tasohyppely.java
@@ -6,5 +6,9 @@ public class Tasohyppely {
     
     public static void main(String[] args) {
         Graphics.init();
+        for(;;){
+        Graphics.drawPlayer(200, 200);
+        Graphics.drawPalikka(100, 50);
+        }
     }
 }
--- src/tasohyppely/graphics/Graphics.java
+++ src/tasohyppely/graphics/Graphics.java
@@ -1,24 +1,35 @@
 package tasohyppely.graphics;
 
+import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public class Graphics {
     private static JFrame ikkuna;
-    private static java.awt.Graphics g;
-    private static final int scale = 16;
+    private static Viewport viewport;
+    private static java.awt.Graphics2D g;
+    private static final int scale = 8;
     
     public static void init() {
-        
+        viewport = new Viewport();
         ikkuna = new JFrame("alahan hyppiä hyvä mies");
         ikkuna.setPreferredSize(new Dimension(800, 600));
         ikkuna.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ikkuna.getContentPane().add(viewport);
         ikkuna.pack();
         ikkuna.setVisible(true);
+        g = (Graphics2D) viewport.getGraphics();
     }
     
-    public static void DrawPlayer(int x, int y) {
+    public static void renderFrame() {
+        viewport.paint(g);
+        viewport.update(g);
+    }
+    
+    public static void drawPlayer(int x, int y) {
         // oikea jalka
         g.drawLine(x + 1 * scale, y, x, y - 2 * scale);
         // vasen jalka
@@ -30,10 +41,24 @@ public class Graphics {
         // vasen käsi
         g.drawLine(x, y - 4 * scale, x - 1 * scale, y - 2 * scale);
         // pää
-        g.drawOval(x, y - 5 * scale, scale, scale);
+        g.drawOval(x - scale / 2, y - 5 * scale, scale, scale);
     }
     
     public static void drawPalikka(int x, int y) {
-        g.drawRoundRect(x, y, 2 * scale, 2 * scale, scale / 4, scale / 4);
+        g.drawRoundRect(x, y, 2 * scale, 2 * scale, scale, scale);
+    }
+    
+    public static class Viewport extends Canvas {
+        public Viewport() {
+            setBackground(Color.WHITE);
+            setSize(600, 500);
+        }
+        
+        public void paint(Graphics g) {
+            drawPlayer(34, 253);
+            drawPalikka(512, 342);
+            //drawPlayer(Player.getX(), Player.getY());
+            //...
+        }
     }
 }
