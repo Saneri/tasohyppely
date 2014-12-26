d2da828eb5e68a6f4f91f8d2a8bd0f57aee2ef57
Saneri <santeri.suitiala@kolumbus.fi>
2014-12-26 21:31:17 +0200

    je

--- src/tasohyppely/logic/Game.java
+++ src/tasohyppely/logic/Game.java
@@ -1,15 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package tasohyppely.logic;
 
-/**
- *
- * @author Santeri
- */
 public class Game {
     
+    private final Map map;
+    
+    public Game(Map map) {
+        this.map = map;
+    }
+    
+    public void start() {
+        
+    }
 }
